61c3867b561050732c66f53c8b51601b23bc1e31
Brian <brianvanstone@gmail.com>
2016-06-21 02:19:00 -0400

    Adding support for jdbc connector to tomcat

--- data-import/src/main/java/tech/notpaper/mws/util/db/connectionmanagement/MySqlConnectionManager.java
+++ data-import/src/main/java/tech/notpaper/mws/util/db/connectionmanagement/MySqlConnectionManager.java
@@ -1,49 +1,38 @@
 package tech.notpaper.mws.util.db.connectionmanagement;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import tech.notpaper.mws.dataimport.Set;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class MySqlConnectionManager extends AbstractConnectionManager {
-	private String host;
-	private String port;
-	private String user;
-	private String pass;
-	private String db;
-	/*
-	static {
-		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-			throw new ConnectionManagerException("Failed to load mysql jdbc driver.", e);
-		}
-	}*/
-	
+
 	public MySqlConnectionManager(String host, String port, String user, String pass, String db) {
-		this.host = host;
-		this.port = port;
-		this.user = user;
-		this.pass = pass;
-		this.db = db;
 		this.conn = buildConnection();
 	}
-	
+
 	public MySqlConnectionManager(String host, String user, String pass, String db) {
 		this(host, null, user, pass, db);
 	}
 
 	@Override
 	protected Connection buildConnection() {
-		String portString = this.port == null ? "" : ":" + this.port;
-		String connectionString = "jdbc:mysql://" + this.host + portString + "/" + this.db + "?" + "user=" + this.user + "&password=" + this.pass;
-		Connection c;
+		Context ctx;
+		DataSource ds;
 		try {
-			c = DriverManager.getConnection(connectionString);
-		} catch (SQLException e) {
-			throw new ConnectionManagerException("Failed to connect to the database.", e);
+			ctx = new InitialContext();
+			ctx = (Context) ctx.lookup("java:comp/env");
+			ds = (DataSource) ctx.lookup("jdbc/mws");
+			if (ds != null) {
+				return ds.getConnection();
+			} else {
+				throw new ConnectionManagerException("Unable to connect to the database.");
+			}
+		} catch (NamingException | SQLException e) {
+			throw new ConnectionManagerException("Unable to connect to the database.", e);
 		}
-		return c;
 	}
 }
